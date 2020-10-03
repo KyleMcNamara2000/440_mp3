a942350ebcd8a43589bce79f7599ca71e48fffa8
kylem <kylemcnamara2000@gmail.com>
2020-10-03 18:38:15 -0500

    fixed bug

--- naive_bayes.py
+++ naive_bayes.py
@@ -79,12 +79,12 @@ def naiveBayes(train_set, train_labels, dev_set, smoothing_parameter=0.01, pos_p
         #probNegative = math.log(float(negativeWords) / totalWords)  # P(negative)
         for word in data:
             newWord = word.lower()
-            if newWord in positiveCounts and positiveCounts[newWord] > lowRange:
+            if newWord in positiveCounts and positiveCounts[newWord] > lowRange and positiveCounts[newWord] < highRange:
                 probPositive += math.log((float(positiveCounts[newWord])+smoothing_parameter)/(positiveWords + smoothing_parameter * (vPos + 1)))
             else:
                 probPositive += math.log(
                     (0.0 + smoothing_parameter) / (positiveWords + smoothing_parameter * 3))
-            if newWord in negativeCounts and negativeCounts[newWord] < highRange:
+            if newWord in negativeCounts and negativeCounts[newWord] > lowRange and negativeCounts[newWord] < highRange:
                 probNegative += math.log((float(negativeCounts[newWord])+smoothing_parameter)/(negativeWords + smoothing_parameter * (vNeg + 1)))
             else:
                 probNegative += math.log(
